8102e6037c023c7739f1364cd9ecaabc137b2c08
davifer21 <davi.fernandescs21@gmail.com>
2024-08-13 09:11:33 -0300

    teste3.txt

--- codigos/resources/views/events/creat.blade.php
+++ codigos/resources/views/events/creat.blade.php
@@ -4,6 +4,15 @@
 
 @section('content')
 
-<h1>crie um evento</h1>
+<div id="event-create-container" class='col-md-6 offset-md-3'>
+    <h1>crie o seu evento</h1>
+    <form action="/events" method="POST">
+    @csrf
+    <div class="form-group">
+      <label for="title">evento:</label>
+      <input type="text" class="form-control">
+    </div>
+    </form>
+</div>
 
-@endsection
+@endsection 
--- codigos/resources/views/welcome.blade.php
+++ codigos/resources/views/welcome.blade.php
@@ -4,10 +4,21 @@
 
 @section("content")
 
-@foreach($events as $event)
-        <p>{{ $event->title }} -- {{ $event->description}}</p>
-@endforeach
+<div id="search-container" class='col-md-12'>
+        <h1>busque um evento</h1>
+        <form action="">
+             <input type="text" id="search" name="seach" class="form-control" placeholder="procurar...">
+        </form>
+</div>
+<div id="events-container" class="col-md-12">
+     <h2>proximos eventos</h2>
+     <p>veja os eventos dos proximos dias</p>
+     <div id="cards-container" class="row">
+        @foreach($events as $event)
+        
+        </div>
 
-@endsection
+        @endforeach
+</div>
 
   
